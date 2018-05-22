2eb9a361301ad8339978afd2d1688ac49dd55e43
Esteban Lopez <esteban.lopez.g@usach.cl>
2018-05-21 23:26:42 -0400

    Version final entregada

--- chatbot.pl
+++ chatbot.pl
@@ -2,7 +2,7 @@
 PARADIGMAS DE PROGRAMACION
 
 CHATBOT HECHO EN PROLOG
-Esteban Andrés López Garrido
+Esteban Andrés Lopez Garrido
 
 Rut: 19837971-9
 Seccion C-3
@@ -10,6 +10,7 @@ Seccion C-3
 Profesor: Daniel Gacitua
 */
 
+%HECHOS
 pelicula("Avengers: Infinity war","ACCION",8.9).
 pelicula("Deadpool2","ACCION",8.2).
 pelicula("Desobediencia","DRAMA",6.7).
@@ -17,7 +18,7 @@ pelicula("Gnomos Al Ataque","INFANTIL",5.5).
 pelicula("Isla de Perros","ANIMACION",7.6).
 pelicula("La Isla De Los Pinguinos","DRAMA",7.8).
 pelicula("La Profecia","TERROR",7.6).
-pelicula("Los Extraños Cacería Nocturna","TERROR",5.6).
+pelicula("Los Extranos Caceria Nocturna","TERROR",5.6).
 pelicula("Rampage: Devastacion","ACCION",5.3).
 pelicula("Sexy Por Accidente","COMEDIA",4.5).
 pelicula("Sherlock Gnomes","INFANTIL",4.9).
@@ -31,7 +32,7 @@ horarioP("Gnomos Al Ataque",[["11:50","14:20"],["14:20","16:30"],["14:20","16:30
 horarioP("Isla de Perros",[["11:40","14:15","16:40"],["14:15","16:40","21:50"],["14:15","16:40","21:50"],["11:40","14:15","16:40"],["14:15","16:40","21:50"],["14:15","16:40","21:50"],["11:40","14:15","16:40"]]).
 horarioP("La Isla De Los Pinguinos",[["14:00"],["14:00"],["14:00"],["14:00"],["14:00"],["14:00"],["14:00"]]).
 horarioP("La Profecia",[["15:00"],["21:00"],["15:00"],["15:00"],["21:00"],["15:00"],["15:00"]]).
-horarioP("Los Extraños: Cacería Nocturna",[["12:30","14:50","17:10"],["12:30","14:50","17:10"],["12:30","14:50","17:10"],["12:30","14:50","17:10"],["12:30","14:50","17:10"],["12:30","14:50","17:10"],["12:30","14:50","17:10"]]).
+horarioP("Los Extranos: Caceria Nocturna",[["12:30","14:50","17:10"],["12:30","14:50","17:10"],["12:30","14:50","17:10"],["12:30","14:50","17:10"],["12:30","14:50","17:10"],["12:30","14:50","17:10"],["12:30","14:50","17:10"]]).
 horarioP("Rampage: Devastacion",[["11:20","14:00"],["14:00","16:40"],["14:00","16:40"],["11:20","14:00"],["14:00","16:40"],["14:00","16:40"],["11:20","14:00"]]).
 horarioP("Sexy Por Accidente",[["11:50","14:20","16:50","19:30"],["14:20","16:50","19:30","22:10"],["14:20","16:50","19:30","22:10"],["11:50","14:20","16:50","19:30"],["14:20","16:50","19:30","22:10"],["14:20","16:50","19:30","22:10"],["11:50","14:20","16:50","19:30"]]).
 horarioP("Sherlock Gnomes",[["10:30","12:45"],["13:50","16:10"],["12:20","14:40"],["10:30","12:45"],["13:50","16:10"],["12:20","14:40"],["10:30","12:45"]]).
@@ -39,22 +40,57 @@ horarioP("Tully",[["13:10","15:50","18:30","21:00"],["13:10","15:50","18:30","21
 horarioP("Yo Soy Simon",[["17:20","19:10"],["12:50","19:10"],["12:50","19:10"],["17:20","19:10"],["12:50","19:10"],["12:50","19:10"],["17:20","19:10"]]).
 
 
-mensaje(formal,0,0,"Hola, en qué lo puedo ayudar?").
+mensaje(formal,0,0,"Hola, en que lo puedo ayudar?").
 mensaje(formal,0,0,"Buenos dias, en que lo puedo ayudar?").
-mensaje(formal,0,0,"Hola, que agradable día, en qué lo puedo ayudar?").
-mensaje(formal,0,1,"Buenas tardes, ¿Qué necesita?").
-mensaje(formal,0,1,"Buenas tardes, ¿Qué puedo hacer por usted?").
-mensaje(formal,0,2,"Buenas noches, ¿Qué necesita?").
-mensaje(formal,0,2,"Que linda que está la luna....¿En qué lo puedo ayudar?").
+mensaje(formal,0,0,"Hola, que agradable dia, en que lo puedo ayudar?").
+mensaje(formal,0,1,"Buenas tardes, ¿Que necesita?").
+mensaje(formal,0,1,"Buenas tardes, ¿Que puedo hacer por usted?").
+mensaje(formal,0,2,"Buenas noches, ¿Que necesita?").
+mensaje(formal,0,2,"Que linda que esta la luna....¿En que lo puedo ayudar?").
 mensaje(formal,1,0,"Digame algun genero que le guste").
-mensaje(formal,1,1,"Le recomiendo la película: {} , tiene una puntuación de {},Hasta Luego !!!").
-mensaje(formal,2,0,"Para que día").
-mensaje(formal,2,1,"¿Qué pelicula desea ver?").
-mensaje(formal,2,2,"Para el día {}, tenemos los siguientes horarios {},Hasta luego !!!").
-mensaje(formal,3,0,"¿Para qué película?").
-mensaje(formal,3,1,"Qué día tiene pensado ir a verla?").
+mensaje(formal,1,0,"Que genero le gusta?").
+mensaje(formal,1,1,"Le recomiendo la pelicula: {} , tiene una puntuacion de {},Hasta Luego !!!").
+mensaje(formal,1,1,"Tengo esta pelicula para usted: {} , tiene una puntuacion de {},Hasta Luego !!!").
+mensaje(formal,2,0,"Para que dia?").
+mensaje(formal,2,0,"Que dia?").
+mensaje(formal,2,1,"Que pelicula desea ver?").
+mensaje(formal,2,1,"Que pelicula quiere ver?").
+mensaje(formal,2,2,"Para el dia {}, tenemos los siguientes horarios {},Hasta luego !!!").
+mensaje(formal,2,2,"El dia {} tenemos los siguientes horarios {},Hasta luego !!!").
+mensaje(formal,3,0,"Para que pelicula?").
+mensaje(formal,3,0,"Que pelicula desea?").
+mensaje(formal,3,1,"Que dia tiene pensado ir a verla?").
+mensaje(formal,3,1,"Que dia?").
 mensaje(formal,3,2,"Bueno, para el dia {} tenemos los siguientes horarios {},¿Que horario escoge?").
-mensaje(formal,3,3,"Ok, su entrada está reservada para el dia {} a las {},Adios...que tenga un lindo día !!!").
+mensaje(formal,3,2,"Bueno, para el dia {} le tengo los siguientes horarios {},¿Que horario escoge?").
+mensaje(formal,3,3,"Ok, su entrada esta reservada para el dia {} a las {},Hasta Luego !!!").
+mensaje(formal,3,3,"Ok, su entrada esta reservada para el dia {} a las {},Adios...que tenga un lindo dia !!!").
+
+mensaje(informal,0,0,"Hola, dime que quieres").
+mensaje(informal,0,0,"Buenos dias,buenas tardes, que quieres").
+mensaje(informal,0,0,"Hola, en que te puedo ayudar?").
+mensaje(informal,0,1,"Buenas tardes, que quieres?").
+mensaje(informal,0,1,"Buenas tardes, que $&$ queri' ? ").
+mensaje(informal,0,2,"Esta brigida la luna... Que necesitai?").
+mensaje(informal,0,2,"Buenas noches, En que te ayudo?").
+mensaje(informal,1,0,"Digame algun genero que te guste").
+mensaje(informal,1,0,"Que genero te gusta?").
+mensaje(informal,1,1,"Te recomiendo la pelicula: {} , tiene una puntuacion de {},Me viro... !!!").
+mensaje(informal,1,1,"Tengo esta pelicula para vo': {} , tiene una puntuacion de {},Bye !!!").
+mensaje(informal,2,0,"Pa que dia?").
+mensaje(informal,2,0,"Que dia queri'?").
+mensaje(informal,2,1,"Que pelicula queri'?").
+mensaje(informal,2,1,"Que pelicula queri' ver?").
+mensaje(informal,2,2,"Pal {}, tenemos los siguientes horarios {},Hasta la vista... !!!").
+mensaje(informal,2,2,"El {} te tengo los siguientes horarios {},Chao pescao !!!").
+mensaje(informal,3,0,"que pelicula queri'").
+mensaje(informal,3,0,"Que pelicula deseas baby?").
+mensaje(informal,3,1,"Que dia tienes pensado ir a verla?").
+mensaje(informal,3,1,"Que dia?").
+mensaje(informal,3,2,"el dia {} estan los siguientes horarios {},cual quieres").
+mensaje(informal,3,2,"Bueno, para el dia {} le tengo los siguientes horarios {},¿Que horario escoge?").
+mensaje(informal,3,3,"Wena, tu entrada esta reservada para el dia {} a las {},Chao pescao !!!").
+mensaje(informal,3,3,"Listo compa , tu entrada esta reservada para el dia {} a las {},Hasta la vista !!!").
 
 
 dia("LUNES").
@@ -104,6 +140,9 @@ horario(KW).
 user1(["Muestrame los horarios","Lunes","sexy por accidente "]).
 user2(["Recomiendame una pelicula","accion"]).
 user3(["comprar una entrada","Desobediencia","Miercoles","18:20"]).
+
+
+%PREDICADOS
 
 /*
 substring1/2
@@ -819,6 +858,8 @@ sendMessage(Msg,Chatbot,InputLog,Seed,OutputLog):-
 	genRespuesta(Msg,Chatbot,OutputLog1,Seed,Respuesta),
 	writeMessage(Msg,"Usuario",OutputLog1,OutputLog2),
 	writeMessage(Respuesta,"Chatbot",OutputLog2,OutputLog),!.
+
+
 
 
 /*
